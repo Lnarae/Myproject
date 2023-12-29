c0842dae2d6175a9f62cea72d9c2ad845a96b870
narae <narae@218.38.137.28>
2023-12-29 13:57:24 +0900

    jsp_prjt v1.1 test

---
+++ jsp_prjt/src/main/java/java_review/Product.java
@@ -0,0 +1,46 @@
+package java_review;
+
+public class Product {
+
+	//DB를 고려해서 클래스 필드 작성
+	//필드는 모두 private으로 정의함
+	/* 필드 정의
+	 제조사: maker - String
+	 CPU 종류: cpuType - String
+	 회면크기: monitorSize - String
+	 램: ram - String
+	 운영체제: os - String
+	 무게: weight - String
+	 저장용량: memory - int
+	 가격: p_price - String
+	 카테고리: category - String
+	 제품명: p_name - String
+	 해상도: resolution - String
+	 CPU이름: cpuName - String
+	 CPU속도: cpuSpeed - String
+	 코어개수: numOFCore - String
+	 램 종류: ramType - String
+	 램 교체여부: ramReplace - int
+	 그래픽 종류: gpuType - String
+	 그래픽 이름: gpuName - String
+	 메모리 종류: memoryType - String
+	 무선랜: wifi - String
+	 영상입출력 단자: videoPort - String
+	 웹캠: webCam - int
+	 USB-C개수: usb_c - int
+	 USB-A개수: usb_a - int
+	 부가기능: add__ons - String
+	 고속충전여부: fastCharging - int
+	 입력장치 부가기능1: input_add_ons1 - String
+	 입력장치 부가기능2: input_add_ons2 - String
+	 입력장치 부가기능3: input_add_ons3 - String
+	 파워: power - String
+	 어댑터: adapter - String
+	 충전단자: chargingPort - String
+	 두께: thickness - String
+	 색상: p_color - String
+	
+	 
+	 
+	 */
+}
